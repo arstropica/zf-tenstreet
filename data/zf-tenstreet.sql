2124490c832d0ba01d0268785e9f131c88632352
ArsTropica <aowilliams@arstropica.com>
2015-08-28 19:31:59 -0400

    Added schema dump.

---
+++ data/zf-tenstreet.sql
@@ -0,0 +1,370 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.12
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 28, 2015 at 07:31 PM
+-- Server version: 5.5.41-MariaDB
+-- PHP Version: 5.6.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `apiuser`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authentication`
+--
+
+CREATE TABLE IF NOT EXISTS `authentication` (
+  `ClientId` int(11) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `form`
+--
+
+CREATE TABLE IF NOT EXISTS `form` (
+  `id` int(11) NOT NULL,
+  `source` varchar(255) NOT NULL,
+  `form` varchar(255) DEFAULT NULL,
+  `companyid` int(11) NOT NULL,
+  `Company` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lead`
+--
+
+CREATE TABLE IF NOT EXISTS `lead` (
+  `id` int(11) NOT NULL,
+  `formid` int(11) NOT NULL,
+  `companyid` int(4) NOT NULL,
+  `company` varchar(255) NOT NULL,
+  `ipaddress` varchar(255) NOT NULL,
+  `referrer` varchar(255) NOT NULL,
+  `formname` varchar(255) NOT NULL,
+  `timecreated` datetime NOT NULL,
+  `timesubmitted` datetime DEFAULT NULL,
+  `submitted` int(1) NOT NULL DEFAULT '0',
+  `driverid` int(11) DEFAULT NULL,
+  `lastresponse` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lead_attributes`
+--
+
+CREATE TABLE IF NOT EXISTS `lead_attributes` (
+  `id` int(11) NOT NULL,
+  `attribute_name` varchar(255) NOT NULL,
+  `attribute_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lead_attribute_values`
+--
+
+CREATE TABLE IF NOT EXISTS `lead_attribute_values` (
+  `id` int(11) NOT NULL,
+  `entity_id` int(11) NOT NULL,
+  `attribute_id` int(4) NOT NULL,
+  `value` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_access_tokens`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_access_tokens` (
+  `access_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_authorization_codes`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_authorization_codes` (
+  `authorization_code` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `redirect_uri` varchar(2000) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL,
+  `id_token` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_clients`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_clients` (
+  `client_id` varchar(80) NOT NULL,
+  `client_secret` varchar(80) DEFAULT NULL,
+  `redirect_uri` varchar(2000) NOT NULL,
+  `grant_types` varchar(80) DEFAULT NULL,
+  `scope` varchar(2000) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_jwt`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_jwt` (
+  `client_id` varchar(80) NOT NULL,
+  `subject` varchar(80) DEFAULT NULL,
+  `public_key` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_refresh_tokens`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_refresh_tokens` (
+  `refresh_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_scopes`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_scopes` (
+  `type` varchar(255) NOT NULL DEFAULT 'supported',
+  `scope` varchar(2000) DEFAULT NULL,
+  `client_id` varchar(80) DEFAULT NULL,
+  `is_default` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_users`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_users` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(2000) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(11) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `roleId` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `displayName` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `apiKey` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(128) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role_linker`
+--
+
+CREATE TABLE IF NOT EXISTS `user_role_linker` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `form`
+--
+ALTER TABLE `form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lead`
+--
+ALTER TABLE `lead`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lead_attributes`
+--
+ALTER TABLE `lead_attributes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lead_attribute_values`
+--
+ALTER TABLE `lead_attribute_values`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `oauth_access_tokens`
+--
+ALTER TABLE `oauth_access_tokens`
+  ADD PRIMARY KEY (`access_token`);
+
+--
+-- Indexes for table `oauth_authorization_codes`
+--
+ALTER TABLE `oauth_authorization_codes`
+  ADD PRIMARY KEY (`authorization_code`);
+
+--
+-- Indexes for table `oauth_clients`
+--
+ALTER TABLE `oauth_clients`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `oauth_jwt`
+--
+ALTER TABLE `oauth_jwt`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `oauth_refresh_tokens`
+--
+ALTER TABLE `oauth_refresh_tokens`
+  ADD PRIMARY KEY (`refresh_token`);
+
+--
+-- Indexes for table `oauth_users`
+--
+ALTER TABLE `oauth_users`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_57698A6AB8C2FD88` (`roleId`),
+  ADD KEY `IDX_57698A6A727ACA70` (`parent_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_1483A5E9E7927C74` (`email`),
+  ADD UNIQUE KEY `UNIQ_1483A5E9F85E0677` (`username`);
+
+--
+-- Indexes for table `user_role_linker`
+--
+ALTER TABLE `user_role_linker`
+  ADD PRIMARY KEY (`user_id`,`role_id`),
+  ADD KEY `IDX_61117899A76ED395` (`user_id`),
+  ADD KEY `IDX_61117899D60322AC` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `form`
+--
+ALTER TABLE `form`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `lead`
+--
+ALTER TABLE `lead`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `lead_attributes`
+--
+ALTER TABLE `lead_attributes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `lead_attribute_values`
+--
+ALTER TABLE `lead_attribute_values`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `role`
+--
+ALTER TABLE `role`
+  ADD CONSTRAINT `FK_57698A6A727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `role` (`id`);
+
+--
+-- Constraints for table `user_role_linker`
+--
+ALTER TABLE `user_role_linker`
+  ADD CONSTRAINT `FK_61117899A76ED395` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `FK_61117899D60322AC` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
