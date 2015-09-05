9fe132e87059c0d7e6ff909debbd3f719069f290
ArsTropica <aowilliams@arstropica.com>
2015-09-05 13:42:02 -0400

    Added Environment specific config options.

--- public/index.php
+++ public/index.php
@@ -2,13 +2,17 @@
 /**
  * Display all errors when APPLICATION_ENV is development.
  */
-if (isset($_SERVER['APPLICATION_ENV']) && $_SERVER['APPLICATION_ENV'] == 'development') {
+$config = 'config/application.config.php';
+if (isset($_SERVER['APPLICATION_ENV']) &&
+		 $_SERVER['APPLICATION_ENV'] == 'development') {
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
+	$config = 'config/application.config.local.php';
 }
 
 /**
- * This makes our life easier when dealing with paths. Everything is relative
+ * This makes our life easier when dealing with paths.
+ * Everything is relative
  * to the application root now.
  */
 chdir(dirname(__DIR__));
@@ -26,4 +30,4 @@ if (php_sapi_name() === 'cli-server') {
 require 'init_autoloader.php';
 
 // Run the application!
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+Zend\Mvc\Application::init(require $config)->run();
